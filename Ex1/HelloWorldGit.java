736c41c1b27c91ba37eb6f9293d93c724cfe2bd2
Gina <Anzo@Athanases-MacBook-Pro.local>
2019-02-12 19:05:32 +0100

    Seconde commit, add Git on name group

--- Ex1/HelloWorldGit.java
+++ Ex1/HelloWorldGit.java
@@ -3,7 +3,7 @@ public class HelloWorldGit {
         String nom = "Tombou";
         String prenom = "Vincent Aubéric";
         String matricule = "000472347";
-        String groupe = "INFO-F307";
+        String groupe = "INFO-F307 git";
 
         System.out.println("Bonjour assistants!");
         System.out.println("Je suis " + prenom + " " + nom + " - " + matricule);
